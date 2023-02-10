307221e208dbd5f538708cf3c4e6116ae1a49580
D4NDR3 <113529922+D4NDR3@users.noreply.github.com>
2023-02-10 10:19:28 -0500

    Add files via upload

--- pygame/click_game.py
+++ pygame/click_game.py
@@ -1,28 +1,33 @@
-spiderpool = Actor('character')
-spiderpool_clicked = Actor('character_clicked')
+import time
+
+spiderpool = Actor("character")
+spiderpool_clicked = Actor("character_clicked")
 
 spiderpool.pos = 250, 40
-
 WIDTH = 500
 HEIGHT = spiderpool.height + 100
 
+
 def draw():
-    screen.fill((255,87,51))
+    screen.fill((100, 0, 0))
     spiderpool.draw()
 
+
 def update():
-        spiderpool.left = spiderpool.left + 5
-        if spiderpool.left> WIDTH:
-            spiderpool.right = 0
+    spiderpool.left = spiderpool.left + 100
+    if spiderpool.left > WIDTH:
+        spiderpool.right = 0
+
+
+def reset_spiderpool():
+    spiderpool.image = "character"
+
 
 def on_mouse_down(pos):
     if spiderpool.collidepoint(pos):
         print("Nice!")
-        spiderpool.image = 'character_clicked'
-        clock.schedule_unique(character, 1.0)
+        spiderpool.image = "character_clicked"
+        clock.schedule_unique(reset_spiderpool, 1.0)
+
     else:
         print("Do better...")
-
-
-
-
